8bfbcd591c42e49b7bd3a08275e93ed82ca4d0b6
Sabiha Imran <sabihaimran@fb.com>
2022-06-16 10:44:44 -0700

    splash screen

---
+++ app/src/main/java/com/example/instagram/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.instagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_background);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 300);
+    }
+}
